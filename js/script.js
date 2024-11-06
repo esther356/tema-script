3070b356e4cd492ab211d71d013c3c9a1995ea42
esther356 <payiesther934@gmail.com>
2024-11-05 22:14:12 -0400

    Update script.js
    Tema

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // script.js
 
-// Este script puede ser utilizado para agregar interactividad a tu página
+// Este script puede ser utilizado para agregar interactividad a la página
 document.addEventListener('DOMContentLoaded', function() {
-    // Puedes agregar funcionalidades como botones, validaciones de formularios, etc.
+    //se  puede agregar funcionalidades como botones, validaciones de formularios, etc.
     console.log('El DOM ha sido completamente cargado');
 });
